405d8629140d1ca76e8851e247b4d2d408842a83
Rauan <138851982+rauan06@users.noreply.github.com>
2024-05-15 23:17:47 +0500

    Fixed the logic of the program

--- generator.py
+++ generator.py
@@ -1,18 +1,19 @@
 from collections import deque
 from random import randint
+import time
 
 # a x a board
 global a
-a = 10
+a = 100
 
 def main():
-    maze = [['0' for _ in range(a)] for _ in range(a)]
+    maze = [['0' for _ in range(a + 2)] for _ in range(a + 2)]
 
-    for i in range(a):
+    for i in range(a + 2):
         maze[0][i] = '1'
         maze[i][0] = '1'
-        maze[i][a - 1] = '1'
-        maze[a - 1][i] = '1'
+        maze[i][a + 1] = '1'
+        maze[a + 1][i] = '1'
     
     # Generation of start and finsih coordinates
     # start_x, start_y = random_coordinate(), random_coordinate()
@@ -27,20 +28,20 @@ def main():
 
     while q:
         x, y, moves, length = q.popleft()
-        if (x, y) == (a - 2, a - 1):
+        if (x, y) == (a, a + 1):
             print(length)
             print(moves)
 
             for dx, dy in moves:
                 maze[dx][dy] = 'W'
+            break
 
 
         if (x, y) not in searched:  
             for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                if 1 <= x + dx < a - 1 and 1 <= y + dy <=  a - 1:
+                if 1 <= x + dx <= a and 1 <= y + dy <= a + 1:
                     q.append((x + dx, y + dy, moves + [(x, y)], length + 1))
                     searched.append((x, y))
-                    maze[x][y] = '1'
 
     with open("maze.txt", "w") as out:
         for row in maze:
