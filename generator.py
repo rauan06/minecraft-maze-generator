035168a8fc9a7808bb0cea27043b4e200213c01b
Rauan <138851982+rauan06@users.noreply.github.com>
2024-05-15 23:22:45 +0500

    Added timers

--- generator.py
+++ generator.py
@@ -7,6 +7,7 @@ global a
 a = 100
 
 def main():
+    start = time.time()
     maze = [['0' for _ in range(a + 2)] for _ in range(a + 2)]
 
     for i in range(a + 2):
@@ -20,18 +21,17 @@ def main():
     # finish_x, finish_y = random_coordinate(), random_coordinate()
     # maze[start_x][start_y] = 'S'
     # maze[finish_x][finish_y] = 'F'
-    
+    finish = time.time()
+    print("Time for initializing:", finish - start, "seconds")
 
     q = deque()
     q.append((1, 1, [(1, 1)], 0))
     searched = []
 
+    start = time.time()
     while q:
         x, y, moves, length = q.popleft()
         if (x, y) == (a, a + 1):
-            print(length)
-            print(moves)
-
             for dx, dy in moves:
                 maze[dx][dy] = 'W'
             break
@@ -42,13 +42,18 @@ def main():
                 if 1 <= x + dx <= a and 1 <= y + dy <= a + 1:
                     q.append((x + dx, y + dy, moves + [(x, y)], length + 1))
                     searched.append((x, y))
+    finish = time.time()
+    print("Time for BFS:", finish - start, "seconds")
 
+
+    start = time.time()
     with open("maze.txt", "w") as out:
         for row in maze:
             for wall in row:
                 out.write(wall)
             out.write("\n")
-
+    finish = time.time()
+    print("Time for writing:", finish - start, "seconds")
 
 def random_coordinate() -> int:
     return randint(1, a - 2)
